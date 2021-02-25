d8a9885d180cc903446b8367a544091f4214487f
Jom Cordova <jmccordova@gmail.com>
2021-02-25 21:30:31 +0800

    Added max k-mer checker

--- Where in the Genome Does Replication Begin/Exer1.2.1.php
+++ Where in the Genome Does Replication Begin/Exer1.2.1.php
@@ -1,10 +1,25 @@
 <?php 
-	if($argc == 2) {
+	/**
+	*	Counts occurrence of a pattern in a genome
+	*	Use in commandline: php Exer1.2.1 <filname/genome> <pattern>
+	*
+	*/
+
+	if($argc != 3) {
 		echo "Expects only two parameters <filename/genome> and <pattern>.";
+		die();
+	} else if ($argc == 3 && (!is_string($argv[1]) || !is_file($argv[1]) || !is_string($argv[2]))) {
+		echo "First parameter should be a file or genome string. Second parameter should be a string pattern.";
 		die();
 	}
 
-	function PatternCount($genome, $pattern) {
+	/**
+	*	Given a genome string and pattern to look for, the function counts how many times the patterns exists in a string.
+	*	@param genome String
+	*	@param pattern String
+	*	@return count Integer
+	*/
+	function patternCount($genome, $pattern) {
 		$count = 0;
 		$genomeLength = strlen($genome);
 		$patternLength = strlen($pattern);
@@ -19,16 +34,16 @@
 	}
 
 	$filename = isset($argv) ? $argv[1] : "./Vibrio_cholerae.txt";
-	$pattern = isset($argv) ? $argv[2] : "GCGCG";
+	$pattern = isset($argv) ? strtoupper($argv[2]) : "GCGCG";
 
 	if(is_file($filename)) {
 		$file = fopen($filename, "r");
 		while(!feof($file)) {
-			$genome = fgets($file);
-			echo "{$pattern} occured " . PatternCount($genome, $pattern) . " time/s.";
+			$genome = strtoupper(fgets($file));
+			echo "{$pattern} occured " . patternCount($genome, $pattern) . " time/s.";
 		}
 		fclose($file);
 	} else {
-		echo "{$pattern} occured " . PatternCount($filename, $pattern) . " time/s.";
+		echo "{$pattern} occured " . patternCount(strtoupper($filename), $pattern) . " time/s.";
 	}
 ?>
---
+++ Where in the Genome Does Replication Begin/Exer1.2.2.php
@@ -0,0 +1,78 @@
+<?php 
+	/**
+	*	Counts occurrence of a pattern in a genome
+	*	Use in commandline: php Exer1.2.1 <filname/genome> <pattern>
+	*
+	*/
+
+	if($argc != 3) {
+		echo "Expects only two parameters <filename/genome> and <length>.";
+		die();
+	} else if ($argc == 3 && (!is_string($argv[1]) || !is_file($argv[1]) || !is_int($argv[2])) && $argv[2] <= 0) {
+		echo "First parameter should be a file or genome string. Second parameter should be a counting number.";
+		die();
+	}
+
+	/**
+	*	Given a genome string and the length of a pattern, the function builds a map of substrings and the number of times the said substring exists.
+	*	@param genome String
+	*	@param patternLength Integer
+	*	@return freqMap AssociativeArray
+	*/
+	function frequencyTable($genome, $patternLength) {
+		$freqMap = array();
+		$genomeLength = strlen($genome);
+		$probeLength = $genomeLength - $patternLength;
+		for($i=0; $i<=$probeLength; $i++) {
+			$pattern = substr($genome, $i, $patternLength);
+			if(!array_key_exists($pattern, $freqMap)) 
+				$freqMap[$pattern] = 1;
+			else
+				$freqMap[$pattern]++;
+		}
+
+		return $freqMap;
+	}
+
+	/**
+	*	After counting, this function keeps the elements that have the highest occurrence; delete if not
+	*	@param freqMap AssociativeArray
+	*	@return freqMap AssociativeArray
+	*/
+	function maxMap($freqMap) {
+		$max = max($freqMap);
+		foreach ($freqMap as $key => $value) {
+			if($value < $max) unset($freqMap[$key]);
+		}
+
+		return $freqMap;
+	}
+
+	function getFrequentWords($genome, $patternLength) {
+		$freqPatterns = array();
+		$freqMap = frequencyTable($genome, $patternLength);
+		$max = maxMap($freqMap);
+
+		return $max;
+	}
+
+	$filename = isset($argv) ? $argv[1] : "./Vibrio_cholerae.txt";
+	$patternLength = isset($argv) ? $argv[2] : 5;
+	$freqMap = array();
+
+	if(is_file($filename)) {
+		$file = fopen($filename, "r");
+		while(!feof($file)) {
+			$genome = fgets($file);
+			$freqMap = getFrequentWords($genome, $patternLength);
+
+		}
+		fclose($file);
+	} else {
+		$freqMap = getFrequentWords($filename, $patternLength);
+	}
+
+	foreach ($freqMap as $key => $value) {
+		echo $key . "\n";
+	}
+?>
